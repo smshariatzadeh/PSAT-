440f7e05f8f069ecae0badd2eff14fe96ac85adb
smshariatzadeh <54735737+smshariatzadeh@users.noreply.github.com>
2023-12-08 14:29:49 +0330

    Add files via upload

--- psat-mat/psat/symfault.m
+++ psat-mat/psat/symfault.m
@@ -1,35 +1,73 @@
 % The program symfault is designed for the balanced three-phase
-% fault analysis of a power system network. The program requires
-% the bus impedance matrix Zbus. Zbus may be defined by the
+% fault analysis of a power system network. 
+%
+% Input:
+% 1)The program requires the bus impedance matrix Zbus.
+% Zbus may be defined by the
 % user, obtained by the inversion of Ybus or it may be
 % determined either from the function Zbus = zbuild(zdata)
 % or the function Zbus = zbuildpi(linedata, gendata, yload).
+%
 % The program prompts the user to enter the faulted bus number
-% and the fault impedance Zf. The prefault bus voltages are
-% defined by the reserved Vector V. The array V may be defined or
-% it is returned from the power flow programs lfgauss, lfnewton,
-% decouple or perturb. If V does not exist the prefault bus voltages
-% are automatically set to 1.0 per unit. The program obtains the
-% total fault current, the postfault bus voltages and line currents.
+% and the fault impedance Zf. 
+%
+% The prefault bus voltages are defined by the reserved Vector V.
+% The array V may be defined or it is returned from the power flow
+% programs. If V does not exist the prefault bus voltages
+% are automatically set to 1.0 per unit.
+%
+% output:
+% The program obtains the total fault current, the postfault bus voltages
+% and line currents.
 %
 % Copyright (C) 1998 H. Saadat
+% edit By S. Majid Shariatzadeh
+% Update:    19-Jul-2023 by smshariatzadeh@yahoo.com
+% Version:   1.0.1+
+%
 
-function symfault(zdata, Zbus, V)
+%function symfault(zdata, Zbus, V)
+function symfault () % no input argument  
 
-fm_var
+fm_var % define PSAT global variables such as setting
 
-if ~autorun('Short Circuit Analysis',0)
+%if ~autorun('Short Circuit Analysis',0)
+%  return
+%end
+
+if isempty(Syn.con) 
+  fm_disp('No generator found', 2) 
   return
+else
+  genData=Syn.con;   
 end
 
+nbus = Bus.n;
 if isempty(Fault.con)
-  fm_disp('No fault found', 2) 
-  return
+  fm_disp('Short Circuit calculations is performed for each bus', 2) 
+  FaultIfm=zeros(1,nbus);
+  FaultIfmang=zeros(1,nbus);  
+  Fault.bus=1:nbus ; % simulate fault in all bus
+  Faultn=nbus;
+  %return
+else
+  FaultIfm=zeros(1,Fault.n); % simulate fault in all faulty bus
+  FaultIfmang=zeros(1,Fault.n);  
+  Faultn=Fault.n;   %number of fault in circuit 
 end
-  
+ 
+%  
+% if exist('V') == 1
+%   if length(V) == nbus
+%     V0 = V;
+%   end
+% else
+%   V0 = ones(nbus, 1) + j*zeros(nbus, 1);
+% end
+
 zdata = Line.con;
-  
-[Zbus, zdata]= zbuildpi(zdata, Syn.con);
+yload =Pl ;
+[Zbus, zdata]= zbuildpi(zdata,genData ,yload);
   
 nl = zdata(:,1); 
 nr = zdata(:,2); 
@@ -44,7 +82,8 @@ end
 ZB = R + j*X
 
 nbr = length(zdata(:,1)); 
-nbus = max(max(nl), max(nr));
+%nbus = max(max(nl), max(nr));
+
 if exist('V') == 1
   if length(V) == nbus
     V0 = V;
@@ -52,22 +91,30 @@ if exist('V') == 1
 else
   V0 = ones(nbus, 1) + j*zeros(nbus, 1);
 end
+
 fprintf('\nThree-phase balanced fault analysis \n')
 
-for ff = 1:Fault.n
+for ff = 1:Faultn
 
-  nf = Fault.bus(ff);    
-  fprintf('Faulted bus No. = %g \n', nf)    
-
-  fprintf('\n Fault Impedance Zf = R + j*X = ')
-  Zf = Fault.con(ff,7) + j*Fault.con(ff,8);
+  nf = Fault.bus(ff); 
+  faultbusname=Bus.names{nf};
+  fprintf('\n\n\n\Faulted bus  = %s \n',faultbusname )    
+  if isempty(Fault.con) 
+      Zf = 0.001;
+  else 
+      Zf = Fault.con(ff,7) + j*Fault.con(ff,8);
+  end
+  
+  fprintf('\nFault Impedance Zf = R + j*X = ')      
   fprintf('%8.5f + j(%8.5f)  \n', real(Zf), imag(Zf))
-  fprintf('Balanced three-phase fault at bus No. %g\n', nf)
-
+  busname=Bus.names{nf};
+  fprintf('Balanced three-phase fault at %s\n\n', busname)
+  
   If = V0(nf)/(Zf + Zbus(nf, nf));
   Ifm = abs(If); 
   Ifmang = angle(If)*180/pi;
-  fprintf('Total fault current = %8.4f per unit \n\n', Ifm)
+  fprintf('Total fault current = %8.4f per unit \n\n\n\n', Ifm)  
+  fprintf('Calculations Detali:\n\n')  
   fprintf('Bus Voltages during fault in per unit \n\n')
   fprintf('     Bus     Voltage       Angle\n')
   fprintf('     No.     Magnitude     degrees\n')
@@ -82,12 +129,12 @@ for ff = 1:Fault.n
       Vfm = abs(Vf(n)); 
       angv=angle(Vf(n))*180/pi;
     end
-    fprintf('   %4g',  n), fprintf('%13.4f', Vfm),fprintf('%13.4f\n', angv)
+    fprintf('   %7s',  Bus.names{n}), fprintf('%13.4f', Vfm),fprintf('%13.4f\n', angv)
   end
   
   fprintf('  \n')
   
-  fprintf('Line currents for fault at bus No.  %g\n\n', nf)
+  fprintf('Line currents for fault at   %s\n\n', faultbusname)
   fprintf('     From      To     Current     Angle\n')
   fprintf('     Bus       Bus    Magnitude   degrees\n')
 
@@ -106,10 +153,10 @@ for ff = 1:Fault.n
           th = angle(Ink);
           %if th <= 0
           if real(Ink) > 0
-            fprintf('      G   '), fprintf('%7g',n), fprintf('%12.4f', Inkm)
+            fprintf('      G   '), fprintf('%7s',Bus.names{n}), fprintf('%12.4f', Inkm)
             fprintf('%12.4f\n', th*180/pi)
           elseif real(Ink) ==0 && imag(Ink) < 0
-            fprintf('      G   '), fprintf('%7g',n), fprintf('%12.4f', Inkm)
+            fprintf('      G   '), fprintf('%7s',Bus.names{n}), fprintf('%12.4f', Inkm)
             fprintf('%12.4f\n', th*180/pi)
           end
           Ign = Ink;
@@ -120,12 +167,12 @@ for ff = 1:Fault.n
           %Ign=Ign+Ink;
           %if th <= 0
           if real(Ink) > 0
-            fprintf('%7g', n)
+            fprintf('%7s', Bus.names{n})
             fprintf('%10g', k),
             fprintf('%12.4f', Inkm)
             fprintf('%12.4f\n', th*180/pi)
           elseif real(Ink) ==0 && imag(Ink) < 0
-            fprintf('%7g', n)
+            fprintf('%7s', Bus.names{n})
             fprintf('%10g', k),
             fprintf('%12.4f', Inkm)
             fprintf('%12.4f\n', th*180/pi)
@@ -135,10 +182,12 @@ for ff = 1:Fault.n
     end
     
     if n == nf  % show Fault Current
-      fprintf('%7g',n)
+      fprintf('%7s',Bus.names{n})
       fprintf('         F')
       fprintf('%12.4f', Ifm)
       fprintf('%12.4f\n', Ifmang)
+	  FaultIfm(n)=Ifm;
+      FaultIfmang(n)=Ifmang;
     end
   end
   resp=0;
@@ -155,4 +204,144 @@ for ff = 1:Fault.n
   
 end   % end for while
 
-fm_disp(['Finished "',filedata,'"']),
+%% generate report
+
+fprintf('  \n')
+if isempty(Fault.con)
+      fprintf('-----------------------------------\n')
+      fprintf('Bus currents for fault at each bus:\n\n')
+
+      fprintf('     From      To     Current     Angle\n')
+      fprintf('     Bus       Bus    Magnitude   degrees\n')
+
+      for n = 1:nbus
+          faultbusname=Bus.names{n};          
+          fprintf('%7s', faultbusname);
+          fprintf('       Ground');
+          fprintf('%12.4f', FaultIfm(n));
+          fprintf('%12.4f\n', FaultIfmang(n));
+
+      end
+
+else
+    
+      nfault=Fault.n;
+      fprintf('-----------------------------------\n')
+      fprintf('Bus currents for fault at selected bus:\n\n')
+
+      fprintf('     From      To     Current     Angle\n')
+      fprintf('     Bus       Bus    Magnitude   degrees\n')
+      
+      for ff = 1:Fault.n;
+          nf = Fault.bus(ff);
+          for n = 1:nbus
+               if n==nf 
+                  faultbusname=Bus.names{n};
+                  fprintf('%7s',faultbusname);
+                  fprintf('       Ground');
+                  fprintf('%12.4f', FaultIfm(n));
+                  fprintf('%12.4f\n', FaultIfmang(n));
+               end   
+          end
+      end  
+
+    
+end 
+fm_disp(['--']),
+
+
+
+  % initialize report structures
+  Header{1,1}{1,1}  = 'PSAT+';
+  Header{1,1}{2,1}  = 'SHORT CIRCUIT REPORT';
+  Header{1,1}{3,1} = ' ';  
+  Cols{1,1}{1,1}= ' '; %no other data
+  Rows{1,1}{1,1} =' ';%no other data
+  Matrix{1,1} = ' ';%no other data
+
+% %  Header{1,1}{3,1} = ['P S A T + ',Settings.version];
+   Header{1,1}{4,1} = ' ';
+   Header{1,1}{5,1} = 'Author:  Majid Shariatzadeh, (c) 2018-2020';
+   Header{1,1}{6,1} = 'e-mail:  m.shariatzadeh@yahoo.com';
+ %  Header{1,1}{7,1} = 'website: faraday1.ucd.ie/psat.html';
+   Header{1,1}{8,1} = ' ';
+   Header{1,1}{9,1} = ['File:  ', Path.data,strrep(File.data,'(mdl)','.mdl')];
+   Header{1,1}{10,1} = ['Date:  ',datestr(now,0)];
+
+
+
+% writing data as a matrix...
+%
+%    MATRIX     Matrix to write to file
+%               Cell array for multiple matrices.
+%    HEADER     String of header information.
+%               Cell array for multiple header.
+%    COLNAMES   (Cell array of strings) Column headers.
+%               One cell element per column.
+%    ROWNAMES   (Cell array of strings) Row headers.
+%               One cell element per row.
+  
+% fm_write(Matrix,Header,Cols,Rows)  
+%fm_write(Matrix,Header,Cols,Rows)  
+       
+if isempty(Fault.con)
+      Header{2,1}{1,1}= '-----------------------------------';
+      Header{2,1}{2,1}= 'Bus currents for fault at each bus:';
+      Header{2,1}{3,1}= ' ';
+      
+
+      Cols{2,1}{1,1}= 'From Bus';
+      Cols{2,1}{1,2}= 'To Bus';      
+      Cols{2,1}{1,3}= 'Current (PU)';            
+      Cols{2,1}{1,4}= 'Angle degrees';                  
+      
+      for n = 1:nbus
+              nf = n;  
+              ff=n;
+              faultbusname=Bus.names{n};          
+              Rows{2,1}{ff,1}= faultbusname;
+              Rows{2,1}{ff,2}= 'Ground';
+              MatrixOut(ff,1)= FaultIfm(n);
+              MatrixOut(ff,2)= FaultIfmang(n);
+
+      end
+      Matrix {2,1}=MatrixOut;
+else
+    
+      nfault=Fault.n;
+      Header{2,1}{1,1}= '-----------------------------------';
+      Header{2,1}{2,1}= 'Bus currents for fault at selected bus:';
+      Header{2,1}{3,1}= ' ';
+ % MatrixOut=zeros(nbus,4);
+      Cols{2,1}{1,1}= 'From Bus';
+      Cols{2,1}{1,2}= 'To Bus';   
+      Cols{2,1}{1,3}= 'Current (PU)';
+      Cols{2,1}{1,4}= 'Angle degrees'; 
+      for ff = 1:Fault.n;
+          nf = Fault.bus(ff);      
+              for n = 1:nbus
+                if n==nf                   
+                  faultbusname=Bus.names{n};          
+                  Rows{2,1}{ff,1}= faultbusname;
+                  Rows{2,1}{ff,2}= 'Ground';
+                  MatrixOut(ff,1)= FaultIfm(n);
+                  MatrixOut(ff,2)= FaultIfmang(n);
+                end
+              end
+      end
+      Matrix {2,1}=MatrixOut;
+
+    
+end             
+fm_write(Matrix,Header,Cols,Rows)    ;   
+       
+
+
+       
+       
+       
+end % end of function
+
+
+
+
